64fd97f5780de43ca11b0c30143a6c5f1ed2d617
鸿则 <hungtcs@163.com>
2020-03-31 13:31:38 +0800

    feat: add webpack-dev-server

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,5 +8,11 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    // proxy: {
+    //   context: ['/api'],
+    //   target: 'http://127.0.0.1:3000'
+    // },
+  },
 });
