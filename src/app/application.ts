3eb238ec143e8cc21f201058aa789acfcf351590
hungtcs <hungtcs@outlook.com>
2022-07-13 15:47:29 +0800

    feat: 🎸 update development log

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,12 +1,7 @@
-import { production } from '@environments';
 
 export class Application {
 
   constructor(container: HTMLElement) {
-    if (!production) {
-      console.warn('Application is running in development mode');
-    }
-
     const element = document.createElement('main');
     element.innerHTML = `
       <h2>Webpack Typescript Starter!</h2>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import './style.scss';
+import { production } from '@environments';
 import { Application } from './app/application';
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept();
+}
+
+if (!production) {
+  console.warn('Application is running in development mode');
 }
 
 async function main() {
